9fa69051367521cea0753478e091c4bf091bae9d
HoneyDeFi <honeydeficontact@gmail.com>
2023-02-03 19:08:23 -0600

    temp remove partnered with component

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,7 +21,7 @@ const Home: NextPage = () => {
       <Liquidation />
       <Stats />
       <HowItWorks />
-      <ParteneredWith />
+      {/* <ParteneredWith /> */}
       <Footer />
     </div>
   )
